599e002c59c6dc054fc396c216311a6d52c6458f
47mango <do_hh@naver.com>
2024-05-27 12:13:53 +0900

    수정

--- models/quser.js
+++ models/quser.js
@@ -1,8 +1,9 @@
 
 const pool = require('../config/db')
 
+//회원가입
 exports.MsignUp = async (data) => {
-    const query = `INSERT INTO user (user_id, user_pw, user_nickname) VALUES (?, ?, ?)`;
+    const query = `INSERT INTO user (user_login_id, user_pw, user_nickname) VALUES (?, ?, ?)`;
     try {
         const [result] = await pool.query(query, [data.id, data.pw, data.username]);
         console.log('Database Insert Result:', result);
@@ -13,24 +14,31 @@ exports.MsignUp = async (data) => {
     }
 };
 
+//로그인
 exports.Mlogin = async (data) => {
-    const query = `SELECT * FROM user WHERE user_id = ?`;
+    const query = `SELECT * FROM user WHERE user_login_id = ?`;
     const [rows] = await pool.query(query, [data.id]);
     return rows;
 };
 
 exports.Minfo = async (id) => {
-    const query = `SELECT * FROM user WHERE user_id = ?`;
+    const query = `SELECT * FROM user WHERE user_login_id = ?`;
     console.log('Executing query:', query, 'with ID:', id); // 로그 추가
     const [rows] = await pool.query(query, [id]);
     return rows;
 };
 
+
+//수정
 exports.Mupdate = async (data) => {
-    const query = `UPDATE user SET user_pw = ?, user_nickname = ? WHERE user_id = ?`;
-    console.log('Executing update query:', query, 'with data:', data); // 로그 추가
+    let query;
+
+    query = `UPDATE user SET user_pw = ?, user_nickname = ?,user_login_id = ? WHERE user_login_id = ?`;
+
+    console.log('Executing update query:', query); // 로그 추가
     try {
-        const [result] = await pool.query(query, [data.pw, data.username, data.id]);
+        console.log(data.pw, data.username, data.id)
+        const [result] = await pool.query(query, [data.pw, data.username, data.id,data.user_id]);
         console.log('Database Update Result:', result); // 로그 추가
         return result;
     } catch (error) {
@@ -38,8 +46,25 @@ exports.Mupdate = async (data) => {
         throw error;
     }
 };
+
+
 exports.Mdelete = async (data) => {
     const query = `DELETE FROM user user_id = ?`;
     const [result] = await pool.query(query, [data.id]);
     return result;
 };
+
+//이미지 경로 저장
+exports.imageup = async (data) => {
+    const query = `INSERT INTO user (user_image) VALUES (?)`
+    const [result] = await pool.query(query,[data.filePath]);
+    return result;
+};
+
+//이미지 조회 getimage
+exports.getimage = async (data) => {
+    const query = `SELECT * FROM image WHERE user_login_id = ?`;
+    const [result] = await pool.query(query,[data.id]);
+    return result;
+};
+
