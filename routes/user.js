077c81825b8e182e0f12f24855b60b63cddc6a3b
47mango <do_hh@naver.com>
2024-05-31 10:49:44 +0900

    라우터

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const userController = require("../controllers/usercontroller"); // 경로 확인
-const authenticateJWT = require("../middlewares/authmiddleware"); // JWT 미들웨어 경로 확인
+const express = require('express');
+const userController = require('../controllers/usercontroller'); // 경로 확인
+const authenticateJWT = require('../middlewares/authmiddleware'); // JWT 미들웨어 경로 확인
 const router = express.Router();
+
 
 router.post('/signUp', userController.CsignUp);
 router.post('/login', userController.Clogin);
-router.get('/info/:id', authenticateJWT, userController.Cinfo);
+router.post('/mypage/:id', authenticateJWT, userController.getUserDetails);
 router.post('/update/:id', authenticateJWT, userController.Cupdate);
 router.delete('/delete', authenticateJWT, userController.Cdelete);
+router.post('/logout', userController.Clogout);
 
 module.exports = router;
