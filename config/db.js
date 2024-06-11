5cc24ad5a4804c9f39105d030fcfe3b0758dc65b
WookJin <wh180911@naver.com>
2024-06-11 14:53:41 +0900

    [test] db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PW,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 20, // 동시에 열 수 있는 최대 연결 수
