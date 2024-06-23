17c517b46d383534a181ca7099df93a0f7d7e529
47mango <do_hh@naver.com>
2024-06-23 18:32:28 +0900

    데이터

--- routes/dthmb.js
+++ routes/dthmb.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DTHcontroller = require('../controllers/DTHcontroller');
+const DTHcontroller = require('../controllers/dthmbcontroller');
 
 router.get('/data/:id',DTHcontroller.getTempData);
 
