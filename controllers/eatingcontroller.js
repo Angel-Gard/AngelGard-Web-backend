2f6617bfafd81b5bea6844948d79a05a219baf97
47mango <do_hh@naver.com>
2024-09-21 19:21:01 +0900

    console제거

--- controllers/eatingcontroller.js
+++ controllers/eatingcontroller.js
@@ -28,26 +28,26 @@ const ydateString = yyear + '-' + ymonth  + '-' + yday;
 
 // 섭취량 입력
 exports.Babyeating = async (req,res) => {
-    console.log(today);
-    console.log(req.body);
+    //console.log(today);
+    //console.log(req.body);
     const {feed_amount,baby_name} = req.body;
-    console.log(feed_amount,baby_name);
+    //console.log(feed_amount,baby_name);
 
     const bName = await BabyM.Selectbabyid(baby_name);
-    console.log("baby_id : ",bName);
+    //console.log("baby_id : ",bName);
 
     if(!bName < 0){
         res.status(400).json({result:false,message:'아이 이름 오류'});
     }else{
         const babyeat = {...req.body,today:today,baby_id:bName};
-        console.log(babyeat);
+        //console.log(babyeat);
 
         if(!feed_amount){
             res.status(400).json({result:false,message:'값을다 입력해 주세요.'});
         }
         else{
             const beating = await EatM.Meating(babyeat);
-            console.log(beating);
+            //console.log(beating);
             if(beating.length < 1){
                 res.status(403).json({ result: false ,message:'실패'});
             }else{
@@ -60,9 +60,9 @@ exports.Babyeating = async (req,res) => {
 
 // 유축량 입력
 exports.Pumping = async (req,res) => {
-    console.log(today);
+    //console.log(today);
     const {intake_amount,baby_name} = req.body;
-    console.log(intake_amount,baby_name);
+    //console.log(intake_amount,baby_name);
 
     const baby_id = await BabyM.Selectbabyid(baby_name);
     
@@ -71,14 +71,14 @@ exports.Pumping = async (req,res) => {
         res.status(400).json({result:false,message:'아이 이름을 찾을 수 없습니다.'});
     }else{
         const intake = {...req.body,today:today,baby_id:baby_id};
-        console.log(intake);
+        //console.log(intake);
 
         if(!intake_amount){
             res.status(400).json({result:false,message:'값을다 입력해 주세요.'});
         }
         else{
             const bintake = await EatM.Mpumping(intake);
-            console.log(bintake);
+            //console.log(bintake);
             if(bintake.length < 1){
                 res.status(403).json({ result: false ,message:'실패'});
             }else{
@@ -102,13 +102,13 @@ exports.InsertMS = async (req,res) => {
     }else{
 
         const ms_time =  {...req.body,today:today,baby_id:baby_id};
-        console.log(ms_time);
+        //console.log(ms_time);
 
         if(!m_time){
             res.status(400).json({result:false,message:'값을다 입력해 주세요.'});
         }else{
             const insetime = await EatM.MMs(ms_time);
-            console.log(insetime);
+            //console.log(insetime);
             if(insetime.length <1){
                 res.status(403).json({ result: false ,message:'실패'});
             }else{
@@ -122,7 +122,7 @@ exports.InsertMS = async (req,res) => {
 
 //섭취량 조회
 exports.SelectEat = async (req,res) => {
-    console.log(req.body);
+    //console.log(req.body);
     const {baby_name} = req.body;
 
     const baby_id = await BabyM.Selectbabyid(baby_name);
@@ -136,8 +136,8 @@ exports.SelectEat = async (req,res) => {
     }else{
         const group_eat = await EatM.MselectEating(today_sel_eat);
         const y_group_eat = await EatM.MselectEating(y_sel_eat);
-        console.log(group_eat);
-        console.log(y_group_eat);
+        //console.log(group_eat);
+        //console.log(y_group_eat);
 
         const total_group_eat = group_eat.reduce((accumulator, current) => {
             return accumulator + current.feed_amount;
@@ -164,7 +164,7 @@ exports.SelectEat = async (req,res) => {
 
 //유축량 조회 
 exports.Selectpum = async (req,res) => {
-    console.log(req.body);
+    //console.log(req.body);
     const {baby_name} = req.body;
 
     const baby_id = await BabyM.Selectbabyid(baby_name);
@@ -177,8 +177,8 @@ exports.Selectpum = async (req,res) => {
     }else{
         const group_pum = await EatM.Mpum(today_sel_pum);
         const y_group_pum = await EatM.Mpum(y_sel_pum);
-        console.log(group_pum);
-        console.log(y_group_pum);
+        //console.log(group_pum);
+        //console.log(y_group_pum);
 
         const total_group_pum = group_pum.reduce((accumulator, current) => {
             return accumulator + current.intake_amount;
@@ -216,8 +216,8 @@ exports.SelectMS = async (req,res) => {
     }else{
         const group_time = await EatM.MselectMS(today_sel_time);
         const y_group_time = await EatM.MselectMS(y_sel_time);
-        console.log(group_time);
-        console.log(y_group_time);
+        //console.log(group_time);
+        //console.log(y_group_time);
 
         const total_group_time = group_time.reduce((accumulator, current) => {
             return accumulator + current.m_time;
