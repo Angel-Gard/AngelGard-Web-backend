4eda4cac1b98cc8acea1254aeca59faadd96ce5f
47mango <do_hh@naver.com>
2024-09-21 19:17:33 +0900

    추가

--- models/quser.js
+++ models/quser.js
@@ -87,3 +87,11 @@ exports.Mdelete = async (data) => {
     return result;
 };
 
+
+exports.Selectbabyid = async (data) => { // baby_id 찾기
+    const query = `SELECT baby_id from baby where baby_name = ?`;
+    const [result] = await pool.query(query,data);
+    console.log(result);
+    return result[0].baby_id;
+}
+
