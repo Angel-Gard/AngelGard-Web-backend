9eff4c5558e16e40025a42235c7007a284461803
47mango <do_hh@naver.com>
2024-10-04 14:51:03 +0900

    pushtest

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
-/*
+
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 
-//const serviceAccount = require('./path/angelguard-push-firebase-adminsdk-mkcok-cce94130f5.json');
+const serviceAccount = require('./path/angelguard-push-firebase-adminsdk-mkcok-cce94130f5.json');
 
 // Firebase 초기화 (위에서 사용한 firebaseConfig와 동일하게 설정)
 
@@ -34,4 +34,3 @@ messaging.onBackgroundMessage((payload) => {
 
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
-*/
