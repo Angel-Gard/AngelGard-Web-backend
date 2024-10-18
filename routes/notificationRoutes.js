671a9ce83a2a858ca40c41845ebd48ddd0804d84
47mango <do_hh@naver.com>
2024-10-18 20:06:10 +0900

    fefad

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -4,7 +4,7 @@ const { sendNotification } = require('../controllers/notificationController');
 const router = express.Router();
 
 // 알림 전송 경로
-//router.get('/send/:uuid', sendNotification);
-router.get('/send/:user_login_id', sendNotification);
+//router.post('/send/:uuid', sendNotification);
+router.post('/send/:user_login_id', sendNotification);
 
 module.exports = router;
