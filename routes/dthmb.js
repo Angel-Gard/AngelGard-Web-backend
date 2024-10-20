5691b7e64244d169196ea0f4e74154f346abed10
47mango <do_hh@naver.com>
2024-10-20 20:56:29 +0900

    eeee

--- routes/dthmb.js
+++ routes/dthmb.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const DTHcontroller = require('../controllers/dthmbcontroller');
 
 router.get('/data/:id',DTHcontroller.getTempData);
+router.post('/test/:user_login_id',DTHcontroller.testcontroller);
 
 module.exports = router;
