1965b67864737ed637d8adcb5647f60cd199ef6f
47mango <do_hh@naver.com>
2024-10-24 13:00:12 +0900

    fdsadfs

--- models/quser.js
+++ models/quser.js
@@ -100,23 +100,11 @@ exports.Selectbabyid = async (data) => { // baby_id 찾기
     
 }
 
-//유저 고유 id 찾기
-exports.SelUid = async (data) => {
-    const query = `SELECT user_id,user_device FROM user WHERE user_login_id = ?`;
-    try{
-        const rows = await pool.query(query,data);
-        console.log('data',rows);
-        return [rows];
-    }catch (error){
-        console.log('Database Query Error : ',error);
-    }
-}
-
 //유저 id로 device값 찾기
 exports.SelDev = async (data) => {
     console.log('넘어온 값',data)
-    // const query = `select user_fcmtoken from user where user_id =? `
-    const query = `select user_fcmtoken from user where user_id =? LIMIT 1`
+    // const query = `select user_fcmtoken from user where user_id =? LIMIT 1`
+    const query = `select user_fcmtoken from user where user_id =?`
     try{
         const [rows] = await pool.query(query,data);
         console.log('data',rows);
