a92ce89be128ee9a779be8b1cfd21f43de110275
47mango <do_hh@naver.com>
2024-10-30 17:11:21 +0900

    fdsafds

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -12,7 +12,7 @@ const sendNotification = async (req, res) => {
     //console.log (user_login_id);
     //const user_id_ = await User.getUniqueUser(user_login_id);
 
-    const user_id = user_id_[0].user_id;
+    const user_id = 1;
     console.log(user_id);
     const uN = user_id_[0].user_id;
     console.log('숫자' , uN);
