5f513277045d76104c28195073e79e0091f2f7dd
47mango <do_hh@naver.com>
2024-11-01 12:58:57 +0900

    dfadfad

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -13,10 +13,10 @@ const sendNotification = async (req, res) => {
     //const user_id_ = await User.getUniqueUser(user_login_id);
     const uN = user_id_[0];
     console.log('숫자' , uN);
-    if(uN== 0){
-      user_id_=5;
-    }
     
+    user_id_=5;
+    
+
     console.log('유저아이디:',user_id_);
 
     const tokenData = await User.SelDev(user_id_);
